05420f03f8267f77b771fd698a9f55fbb44d6f6c
phananh85327 <phananh85327@gmail.com>
2025-06-12 22:51:59 +0700

    Update

--- Data/Constants.cs
+++ Data/Constants.cs
@@ -9,7 +9,7 @@
         public const string PROLOG_FILE_NAME = "expert.pl";
         public const string CLUSTER_FILE_NAME = "cluster.csv";
         public const string DATASET_FILE_NAME = "dataset.csv";
-        public const string SWI_FILE_PATH = "";
+        public const string SWI_FILE_PATH = "D:\\00 - Project\\ExpertSmartphone\\swipl\\bin\\swipl-win.exe";
         public const string PYTHON_VENV = "Python/.venv/Scripts/python.exe";
         public const string PYTHON_KMEANS_SCRIPT_FILE_PATH = "Python/main.py";
     }
